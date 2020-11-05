ca3bbe7fe6f03647b03aacde08676c3781966f20
Arun Kumar <arungnaneshwaran@gmail.com>
2020-11-06 00:40:04 +0530

    null

---
+++ Week_1/JavaScript Practice problems in JSON(Objects) and List/Problem_6.js
@@ -0,0 +1,10 @@
+var arr= [[['firstName', 'Vasanth'], ['lastName', 'Raja'], ['age', 24], ['role', 'JSWizard']], 
+            [['firstName', 'Sri'], ['lastName', 'Devi'], ['age', 28], ['role', 'Coder']]];
+function transformEmployeeData(arr) {
+ var tranformEmployeeList = [];
+ 
+ //Your code
+ 
+ 
+ return tranformEmployeeList;
+}
