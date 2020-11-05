4b971ada7daada325ec6eb28c3397aaab4f10bd1
Arun Kumar <arungnaneshwaran@gmail.com>
2020-11-05 22:56:50 +0530

    Final version

--- Week_1/JavaScript Practice problems in JSON(Objects) and List/Problem_0 _Part A.js
+++ Week_1/JavaScript Practice problems in JSON(Objects) and List/Problem_0 _Part A.js
@@ -18,15 +18,39 @@ var cat = {
    console.log(cat);
 
 //1.Add height and weight to Fluffy
-//2.Fluffy name is spelled wrongly. Update it to Fluffyy
-//3.List all the activities of Fluffyy’s catFriends.
-//4.Print the catFriends names.
-//5.Print the total weight of catFriends
-//6.Print the total activities of all cats (op:6)
-//7.Add 2 more activities to bar & foo cats
-//8.Update the fur color of bar
-
 cat.height = 5;
 cat.weight = 10;
+
+//2.Fluffy name is spelled wrongly. Update it to Fluffyy
 cat.name = "Fluffyy";
-console.log(cat.catFriends.activities)
+
+//3.List all the activities of Fluffyy’s catFriends.
+let catActivities=[];
+for (let key in cat.catFriends){
+    catActivities = catActivities.concat(cat.catFriends[key].activities);
+}
+console.log(catActivities);
+
+//4.Print the catFriends names.
+let catFriendsname=[]
+for (key in cat.catFriends){
+    catFriendsname = catFriendsname.concat(cat.catFriends[key].name);}
+
+console.log(catActivities);
+
+//5.Print the total weight of catFriends
+let catFriendsweight=0
+for (key in cat.catFriends){
+    catFriendsweight += cat.catFriends[key].weight);}
+
+console.log(catFriendsweight);
+
+//6.Print the total activities of all cats (op:6)
+
+
+//7.Add 2 more activities to bar & foo cats
+
+
+//8.Update the fur color of bar
+
+
